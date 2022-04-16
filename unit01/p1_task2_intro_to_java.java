13d004625fc4a6158f379e4097df08b9018f98a5
samarth nayal <samarthnayal87@gmail.com>
2022-04-16 09:52:23 +0530

    initial commit

---
+++ unit01/p1_task2_intro_to_java.java
@@ -0,0 +1,7 @@
+package unit01;
+
+public class p1_task2_intro_to_java {
+    public static void main(String[] args) {
+        System.out.println("hello world its the beginning not the end");
+    }
+}
